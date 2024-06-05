9c95f6f72219b9195f22d7171b6da96faba70345
RakaAgiSaputra <Raka_Agi_Saputra@teknokrat.ac.id>
2024-06-06 04:02:39 +0700

    raka: transaction HomeFragment dengan fragment ContentFragment

--- program/coralConnect/app/src/main/java/com/uti/coralconnect/HomeFragment.kt
+++ program/coralConnect/app/src/main/java/com/uti/coralconnect/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.Group
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +31,26 @@ class HomeFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val btnAll: Button = view.findViewById(/* id = */ R.id.category_btn_all)
+
+        val fragment = HomeFragment()
+        btnAll.setOnClickListener{
+            replaceFragment(ContentFragment())
+        }
+        return view
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.display_content, fragment).addToBackStack(null).commit()
     }
 
     companion object {
@@ -56,4 +72,6 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+
 }
