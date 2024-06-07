e84d464560ea8e209470cf574ec45d27dbbf1e5d
RakaAgiSaputra <Raka_Agi_Saputra@teknokrat.ac.id>
2024-06-07 12:35:50 +0700

    raka: menambahkan tampilan login dan register

---
+++ program/coralConnect/app/src/main/java/com/uti/coralconnect/database/model/TaskListModel.kt
@@ -0,0 +1,7 @@
+package com.uti.coralconnect.database.model
+
+class TaskListModel {
+    var id : Int = 0
+    var name : String = ""
+    var details : String = ""
+}
--- program/coralConnect/app/src/main/java/com/uti/coralconnect/fragment/HomeFragment.kt
+++ program/coralConnect/app/src/main/java/com/uti/coralconnect/fragment/HomeFragment.kt
@@ -2,14 +2,17 @@ package com.uti.coralconnect.fragment
 
 import android.graphics.Color
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
-import com.uti.coralconnect.ARG_PARAM1
-import com.uti.coralconnect.ARG_PARAM2
 import com.uti.coralconnect.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
